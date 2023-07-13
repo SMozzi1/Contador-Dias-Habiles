4b9f467da1d8300c482f8cbd9cb526e71af5a6ae
SMozzi <santimozzi2@gmail.com>
2023-07-13 19:22:57 -0300

    correccion de boton y correcion de inicio de cuenta de fechas

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ function stepper(btn){
     let min = myInput.getAttribute("min");
     let max = myInput.getAttribute("max");
     let step = myInput.getAttribute("step");
-    let val = myInput.getAttribute("value");
+    let val = myInput.value;
     let calcStep = (id == "increment") ? (step*1) : (step * -1);
     let newValue = parseInt(val) + calcStep;
 
     if(newValue >= min && newValue <= max){
-        myInput.setAttribute("value", newValue);
+        myInput.value = newValue;
     }
 }
 
@@ -43,6 +43,7 @@ document.querySelector(".boton").addEventListener("click", function(){
     if(dateRegex.test(fechaInicio)){
         let diasContados = 0;
     let fechaActual = new Date(fechaInicio);
+    fechaActual.setDate(fechaActual.getDate() + 1)
     let diasFeriadosEncontrados = [];
     while (diasContados < diasHabiles) {
        
